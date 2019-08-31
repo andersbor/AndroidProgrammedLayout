2612fa1edd228c7ae1c94618d51f7e50344fb1ed
Anders Børjesson <anbo@zealand.dk>
2019-08-31 08:03:28 +0200

    initial

---
+++ app/src/main/java/dk/easj/anbo/programmedlayout/FullyProgrammedLayoutActivity.java
@@ -0,0 +1,56 @@
+package dk.easj.anbo.programmedlayout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class FullyProgrammedLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_fully_programmed_layout);
+        // XML layout file NOT used
+
+        ViewGroup.LayoutParams params =
+                new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        this.addContentView(layout, params);
+
+        TextView textView = new TextView(this);
+        textView.setText("This Activity is totally programmed. No layout XML file");
+        textView.setLayoutParams(params);
+        layout.addView(textView);
+
+        final EditText editText = new EditText(this);
+        editText.setHint("Number of buttons");
+        editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+        editText.setLayoutParams(params);
+        layout.addView(editText);
+
+        Button button = new Button(this);
+        button.setText("Next");
+        button.setLayoutParams(params);
+        layout.addView(button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), PartiallyProgrammedLayoutActivity.class);
+                String numberStr = editText.getText().toString();
+                int numberOfButtons = Integer.parseInt(numberStr);
+                intent.putExtra(PartiallyProgrammedLayoutActivity.NUMBER_OF_BUTTONS, numberOfButtons);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/dk/easj/anbo/programmedlayout/PartiallyProgrammedLayoutActivity.java
@@ -0,0 +1,42 @@
+package dk.easj.anbo.programmedlayout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class PartiallyProgrammedLayoutActivity extends AppCompatActivity {
+
+    public static final String NUMBER_OF_BUTTONS = "NUMBER_OF_BUTTONS";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partially_programmed_layout);
+
+        int defaultValue = 1;
+        Intent intent = getIntent();
+        int howManyButtons = intent.getIntExtra(NUMBER_OF_BUTTONS, defaultValue);
+
+        ViewGroup layout = findViewById(R.id.partiallyProgrammedLayout);
+        ViewGroup.LayoutParams params =
+                new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+        for (int i = 1; i <= howManyButtons; i++) {
+            final int buttonNumber = i;
+            Button button = new Button(this);
+            button.setText("Buttton " + i);
+            layout.addView(button, params);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getBaseContext(), "You clicked button " + buttonNumber, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
